12c76c8ebeeea5e0d5063dbed813da09d0299922
vls6 <mress@tutanota.com>
2020-11-16 21:17:48 +0100

    initial commit

---
+++ src/main/java/com/kodilla/edgeserver/KodillaEdgeServerApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.edgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaEdgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaEdgeServerApplication.class, args);
+	}
+
+}
